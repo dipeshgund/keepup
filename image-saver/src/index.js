06459016f0f83368f42b0efc5953988fe303d927
Dipesh Gund <dipeshgund229@gmail.com>
2021-03-17 20:02:05 +0530

    Background added

--- image-saver/src/index.js
+++ image-saver/src/index.js
@@ -2,7 +2,7 @@ import App from './App';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import store from './store/store';
-
+import './index.css';
 ReactDOM.render(
     <Provider store ={store}>
         <App/>
